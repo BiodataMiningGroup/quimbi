BiodataMiningGroup/quimbi

commit 59e46f4de664be9c1b30b64613010a95b5336d53

Cleaned code, added comments

src/components/DisplayData.vue
@@ -26,10 +26,12 @@
 </template>
 
 <script>
+    // Child Compontents
     import Histogram from './Histogram.vue'
     import Spectrum from './Spectrum.vue'
     import ColorScale from './ColorScale.vue'
 
+    // OpenLayers
     import Map from '../../node_modules/ol/Map';
     import View from '../../node_modules/ol/View';
     import Feature from '../../node_modules/ol/Feature';
@@ -43,6 +45,7 @@
     import Style from '../../node_modules/ol/style/Style';
     import Projection from 'node_modules/ol/proj/Projection';
     import {getCenter} from 'node_modules/ol/extent';
+
 
     import RenderHandler from '../utils/RenderHandler.js';
 
@@ -78,6 +81,10 @@
                 markerBorderStyle: {}
             }
         },
+        /**
+         * Called before screen is rendered. Inits the Renderhandler,
+         * retrievs colormap array and bounds for the colormap scale
+         */
         created() {
             // Initialize shader
             this.renderHandler = new RenderHandler(this.data);
@@ -85,14 +92,22 @@
             this.colormapvalues = this.renderHandler.colorMap.getColorMapValues();
             this.bounds = this.renderHandler.intensityHistogram.bounds;
         },
+        /**
+         * Called after created(), when dom elements are accessible
+         */
         mounted() {
             // Create map view after html template has loaded
             this.createMap();
             // Draw Color-Scale
             this.$refs.scaleCanvas.redrawScale();
         },
+
         methods: {
 
+            /**
+             * Created the openlayers map with the glmvilib canvas as layer source, adds hidden marker for
+             * pixel selection and calls updateView() to watch for mouse movement
+             */
             createMap() {
                 let extent = [0, 0, this.data.canvas.width, this.data.canvas.height];
                 let projection = new Projection({
@@ -125,21 +140,21 @@
                 this.createMarker();
                 this.map.addLayer(this.markerLayer);
 
-                // Set values for the Intensity Histogram and Color-Scale
-                this.histogramData = this.renderHandler.intensityHistogram.histogram;
-                this.bounds = this.renderHandler.intensityHistogram.bounds;
-
                 // Render and update image on mouse movement
                 this.updateView();
 
             },
 
+            /**
+             * Helper to create the marker which gets visible by clicking on a pixel
+             */
             createMarker() {
                 // Create marker for click event
                 let vectorSource = new VectorSource({
                     features: []
                 });
 
+                // Inner white circle
                 this.markerStyle = new Style({
                     image: new Circle({
                         radius: 4,
@@ -149,6 +164,7 @@
                     })
                 });
 
+                // Outer black circle to create border effect around the white circle
                 this.markerBorderStyle = new Style({
                     image: new Circle({
                         radius: 6,
@@ -165,12 +181,15 @@
                     style: [this.markerBorderStyle, this.markerStyle]
                 });
                 vectorSource.addFeature(this.markerFeature);
-                // Hide marker
+                // Initially hide marker
                 this.markerLayer.setVisible(false);
             },
 
+            /**
+             * Adds event listener for map interaction
+             */
             updateView() {
-                // Render image to show it before user moves the mouse
+                // Render image once to show something before the mouse is being moved
                 this.renderHandler.render(this.mouse);
                 this.map.render();
 
@@ -195,6 +214,11 @@
                 });
             },
 
+            /**
+             * Called on free mouse movement. Adds small delay between mouse movement events to prevent lag
+             * caused by too much rendering
+             * @param event
+             */
             updateOnMouseMove(event) {
                 if (!this.markerIsActive) {
                     // Update if there is a certain time interval (in ms) between movements
@@ -207,6 +231,10 @@
 
             },
 
+            /**
+             * Sets marker if mouse is clicked, activates button in toolbar, rerenders the image and
+             * gets values for the spectrum view
+             **/
             updateOnMouseClick(event) {
                 // Set Marker position
                 this.markerFeature.getGeometry().setCoordinates([(Math.floor(event.coordinate[0]) + 0.5),
@@ -216,7 +244,7 @@
                     this.markerIsActive = true;
                 }
                 this.updateMousePosition(event);
-                // Todo wohin damit?
+                // Todo refactor please
                 this.renderHandler.selectionInfo.updateMouse(this.mouse.x, this.mouse.y);
                 glmvilib.render.apply(null, ['selection-info']);
                 this.renderHandler.framebuffer.updateSpectrum();
@@ -224,7 +252,10 @@
 
             },
 
-            // Updates relative mouse position and rerenders the map
+            /**
+             * Sets relative mouse position and rerenders the map, histogram and color scale
+             * @param event
+             */
             updateMousePosition(event) {
                 // Norm x and y values and prevent webgl coordinate interpolation
                 this.mouse.x = (Math.floor(event.coordinate[0]) + 0.5) / this.data.canvas.width;
@@ -237,6 +268,9 @@
                 }
             },
 
+            /**
+             * Well, toggles the marker on map and toolbar
+             */
             toggleMarker() {
                 // Deactivate Marker
                 if (this.markerIsActive) {
@@ -246,6 +280,10 @@
                 }
                 this.markerIsActive = true;
             },
+
+            /**
+             * Called to update the histogram and color scale in the ui
+             */
             updateHistogram() {
                 this.$refs.histogram.redrawHistogram();
                 this.$refs.scaleCanvas.redrawScale();

src/App.vue
@@ -24,7 +24,10 @@
             }
         },
         methods: {
-            // Gets called when data was initialized by the InitData component
+            /**
+             * Gets called when data was initialized by the InitData component
+             * @param data
+             */
             onFinishedInit(data) {
                 this.showInitData = false;
                 this.showDisplayData = true;

src/assets/styles/styles.css
@@ -1,8 +1,9 @@
+/* Let document fit screen and set dark background color */
 html {
     height: 100%;
     background-color: #1F1F1F;
 }
-
+/* Styles for openlayer zooming buttons */
 .ol-control button {
     background-color: rgba(96, 96, 96, 0.5) !important;
 }
@@ -12,10 +13,12 @@ html {
     border-color: #131313;
 }
 
+/* Text color being used */
 body {
     color: #e8e8e8;
 }
 
+/* Styling for spectrum y-axis horizontale lines */
 .yaxis .tick line {
     stroke: grey;
     stroke-opacity: 0.3;

src/components/ColorScale.vue
@@ -31,6 +31,9 @@
         mounted() {
         },
         methods: {
+            /**
+             * Draws the color scale with scale factor to the colorscale canvas and positions it correctly with css
+             */
             redrawScale() {
                 this.scaleFactor = this.bounds[1] - this.bounds[0] || 1;
                 this.ctx = document.getElementById('colorscale').getContext("2d");
@@ -39,19 +42,20 @@
                 this.ctx.clearRect(0, 0, width, height);
                 let drawnumber = height;
                 let index = 0;
-                while(drawnumber) {
-                    this.ctx.fillStyle = "rgb(" + this.colormapvalues[index] + "," + this.colormapvalues[index + 1] +  "," + this.colormapvalues[index + 2] + ")";
+                // Draw colored lines
+                while (drawnumber) {
+                    this.ctx.fillStyle = "rgb(" + this.colormapvalues[index] + "," + this.colormapvalues[index + 1] + "," + this.colormapvalues[index + 2] + ")";
                     this.ctx.fillRect(0, drawnumber, 30, 1);
                     index += 3;
-                    drawnumber--;console.log();
+                    drawnumber--;
                 }
                 // If no scaling is applied, set height op top wrapper to zero
-                if(this.scaleFactor === 1) {
+                if (this.scaleFactor === 1) {
                     document.getElementById('wrapper-top').style = "height: 0px";
                 } else {
-                    document.getElementById('wrapper-top').style = "height: " +  (256 - Math.floor(this.bounds[1] * 256)) + "px";
+                    document.getElementById('wrapper-top').style = "height: " + (256 - Math.floor(this.bounds[1] * 256)) + "px";
                 }
-                document.getElementById('wrapper-bottom').style = "height: " +  Math.floor(this.bounds[0] * 256) + "px";
+                document.getElementById('wrapper-bottom').style = "height: " + Math.floor(this.bounds[0] * 256) + "px";
                 document.getElementById('colorscale').style = "transform: scaleY(" + this.scaleFactor + "); transform-origin:top;";
             },
         }
@@ -83,8 +87,9 @@
         padding: 0;
         border-bottom: grey dotted 1px;
     }
+
     #wrapper-bottom {
-        background-color:black;
+        background-color: black;
         margin: 0;
         padding: 0;
         width: 30px;

src/components/Histogram.vue
@@ -22,6 +22,10 @@
         mounted() {
         },
         methods: {
+            /**
+             * Draws the histogram on a canvas element with a fixed width of 40 and a height equivalent to
+             * the number of colors in the color map (256)
+             */
             redrawHistogram() {
                 this.ctx = document.getElementById('histogram').getContext("2d");
                 let width = 40;

src/components/InitData.vue
@@ -53,7 +53,9 @@
             }
         },
         watch: {
-            // Loads glmvlib if all images are loaded
+            /**
+             *  Loads glmvlib if all images are loaded
+             **/
             counter() {
                 this.progressBar = Math.round((this.counter / this.data.files.length * 100));
                 if (this.counter === this.data.files.length) {
@@ -148,9 +150,13 @@
 
             },
 
+            /**
+             * Inits the glmbilib with parsed data
+             */
             initGlmvlib() {
                 try {
                     window.glmvilib.init(
+                        // Canvas element for openlayers
                         this.data.canvas,
                         {
                             width: this.data.dataWidth,
@@ -160,20 +166,17 @@
                         }
                     );
 
+                    // Write tiles row major into textures
                     window.glmvilib.storeTiles(this.data.images);
                     this.loading = false;
 
+                    // Emit finish message to switch to switch component
                     this.$emit('finish', this.data)
-                    // Todo shader.getActive()
-                    // Todo glmvilib.render.apply(null, [a,b,c])
-                    // Todo start with angledist shader, Programm.coffee,
-                    // services/shader.coffee : createProgramms(), getActive()
-                    // renderer.coffee: update() glmvilib.render(shader.getActive())
-                    // glmbilib.render('angle-dist');s
 
+                    // Never happens, but better be save than sorry
                 } catch (error) {
-                    // Todo show error on gui
                     console.log(error);
+                    // Kill the library to release memory
                     window.glmvilib.finish();
                 }
             }
@@ -216,11 +219,9 @@
     }
 
     h1 {
-        font-family: Helvetica,sans-serif;
+        font-family: Helvetica, sans-serif;
         font-weight: bold;
         font-size: 1.7em;
     }
-
-
 
 </style>

src/components/Spectrum.vue
@@ -43,19 +43,21 @@
                 clearedXValues: [],
                 lineColor: '#e8e8e8',
                 tickXValues: [],
-                // Zooom Factor of Spectrum when Points get visible
+                // Zoom Factor of Spectrum when Points get visible
                 zoomFactorPoints: 2
             }
         },
+        /**
+         * Created the graph and draw it once without values not zoomed
+         */
         mounted() {
-
-            this.initCanvas();
+            this.initGraph();
             this.drawSpectrum(d3.zoomIdentity);
 
         },
         methods: {
 
-            initCanvas() {
+            initGraph() {
 
                 this.doDomCalculations();
 
@@ -101,6 +103,8 @@
                     .attr("class", "yaxis")
                     .call(this.yAxis);
                 this.ctx = this.canvas.node().getContext('2d');
+
+                // Add event listener for user interaction
                 this.canvas.call(this.zoomSpectrum());
 
                 // Resize spectrum if window size changes
@@ -172,6 +176,9 @@
 
             },
 
+            /**
+             * Draws spectrum when user zooms or moves the graph
+             */
             redrawSpectrum() {
                 this.normYValues();
                 // Redraw spectrum with current zoom
@@ -179,6 +186,9 @@
 
             },
 
+            /**
+             * Called on user interaction
+             */
             zoomSpectrum() {
                 return d3.zoom().scaleExtent([1, 100]).translateExtent([[0, 0], [this.canvasWidth, this.canvasHeight]]).extent([[0, 0], [this.canvasWidth, this.canvasHeight]])
                     .on('zoom', () => {
@@ -189,11 +199,18 @@
                     });
             },
 
+            /**
+             * Norms the intensity values which are 0 to 255 to relative intensity values between 0 and 100
+             */
             normYValues() {
                 let maxY = this.findMaxYValue();
                 this.normedYValues = this.yValues.map(val => val / maxY * 100);
             },
 
+            /**
+             * Helper to find highest value for norming
+             * @returns {number}
+             */
             findMaxYValue() {
                 let maxValue = 0;
                 this.yValues.forEach(val => {
@@ -204,6 +221,10 @@
                 return maxValue;
             },
 
+            /**
+             * Makes graph responsive.
+             * Recalculates the graph dom objects to fit screen, if it is rescaled by the user.
+             */
             fitToScreen() {
 
                 this.doDomCalculations();
@@ -233,6 +254,9 @@
                 this.redrawSpectrum();
             },
 
+            /**
+             * Updates dom elements to current values, before new screen rescale
+             */
             doDomCalculations() {
                 this.spectrumAxes = document.getElementById('spectrum-axes');
                 this.canvasWidth = document.getElementById('spectrum-axes').offsetWidth - this.spectrumMargin.left - this.spectrumMargin.right;
@@ -256,6 +280,5 @@
     #spectrum-canvas {
         position: absolute;
     }
-
 
 </style>

commit f764c790ab63a401e3fd5ad6631d8c7a53144e01

Fix color scale and histogram
They broke in 59e46f4de664be9c1b30b64613010a95b5336d53

src/components/DisplayData.vue
@@ -70,7 +70,7 @@
                     y: 0
                 },
                 histogramData: [],
-                bounds: {},
+                bounds: [],
                 colorMapData: {},
                 markerIsActive: false,
                 viewMode: 'similarity',
@@ -90,6 +90,7 @@
             this.renderHandler = new RenderHandler(this.data);
             this.renderHandler.createShader();
             this.colormapvalues = this.renderHandler.colorMap.getColorMapValues();
+            this.histogramData = this.renderHandler.intensityHistogram.histogram;
             this.bounds = this.renderHandler.intensityHistogram.bounds;
         },
         /**