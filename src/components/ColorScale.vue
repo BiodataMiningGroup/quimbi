c85bb8028eb3a2beb204756e83bed177fe0fc62c
Martin Zurowietz <martin@zurowietz.de>
2019-01-08 14:34:33 +0100

    Improve color scale

--- src/components/ColorScale.vue
+++ src/components/ColorScale.vue
@@ -1,9 +1,8 @@
 <template>
-    <div id="colorscale-wrapper">
-        <div id="wrapper-top"></div>
-        <canvas id="colorscale" ref="canvas" width="30" height="256">
-        </canvas>
-        <div id="wrapper-bottom"></div>
+    <div class="colorscale">
+        <div class="wrapper-top" ref="wrapperTop"></div>
+        <canvas class="canvas" ref="canvas"></canvas>
+        <div class="wrapper-bottom" ref="wrapperBottom"></div>
     </div>
 
 </template>
@@ -28,73 +27,74 @@
                 scaleFactor: 1,
             }
         },
-        mounted() {
-        },
         methods: {
             /**
              * Draws the color scale with scale factor to the colorscale canvas and positions it correctly with css
              */
             redrawScale() {
                 this.scaleFactor = this.bounds[1] - this.bounds[0] || 1;
-                this.ctx = document.getElementById('colorscale').getContext("2d");
-                let width = 30;
-                let height = 256;
-                this.ctx.clearRect(0, 0, width, height);
-                let drawnumber = height;
+                this.ctx = this.$refs.canvas.getContext("2d");
+                let height = 0;
+                // If no scaling is applied, set height op top wrapper to zero
+                if (this.scaleFactor === 1 || this.bounds[1] === 1) {
+                    this.$refs.wrapperTop.style = "display: none;";
+                } else {
+                    height = (1 - this.bounds[1]) * 100;
+                    this.$refs.wrapperTop.style = "height: " + height + "%";
+                }
+
+                if (this.scaleFactor === 1 || this.bounds[0] === 0) {
+                    this.$refs.wrapperBottom.style = "display: none;";
+                } else {
+                    height = this.bounds[0] * 100;
+                    this.$refs.wrapperBottom.style = "height: " + height + "%";
+                }
+                height = this.scaleFactor * 100;
+                this.$refs.canvas.style = "height: " + height + "%;";
+            },
+
+            initScale() {
+                let height = this.colormapvalues.length / 3;
+                this.ctx = this.$refs.canvas.getContext("2d");
+                this.$refs.canvas.width = 1;
+                this.$refs.canvas.height = height;
                 let index = 0;
                 // Draw colored lines
-                while (drawnumber) {
+                for (let i = height - 1; i >= 0; i--) {
+                    index = i * 3;
                     this.ctx.fillStyle = "rgb(" + this.colormapvalues[index] + "," + this.colormapvalues[index + 1] + "," + this.colormapvalues[index + 2] + ")";
-                    this.ctx.fillRect(0, drawnumber, 30, 1);
-                    index += 3;
-                    drawnumber--;
+                    this.ctx.fillRect(0, height - (i + 1), 1, 1);
                 }
-                // If no scaling is applied, set height op top wrapper to zero
-                if (this.scaleFactor === 1) {
-                    document.getElementById('wrapper-top').style = "height: 0px";
-                } else {
-                    document.getElementById('wrapper-top').style = "height: " + (256 - Math.floor(this.bounds[1] * 256)) + "px";
-                }
-                document.getElementById('wrapper-bottom').style = "height: " + Math.floor(this.bounds[0] * 256) + "px";
-                document.getElementById('colorscale').style = "transform: scaleY(" + this.scaleFactor + "); transform-origin:top;";
-            },
-        }
+            }
+        },
+        mounted() {
+            this.initScale();
+        },
     }
 
 </script>
 
 
 <style scoped>
-
-    #colorscale {
-        margin: 0;
-        padding: 0;
-
-    }
-
-    #colorscale-wrapper {
-        margin: 0;
-        padding: 0;
+    .colorscale {
+        display: flex;
+        flex-direction: column;
         width: 30px;
         height: 256px;
-        vertical-align: top;
         position: relative;
     }
 
-    #wrapper-top {
-        background-color: white;
-        margin: 0;
-        padding: 0;
-        border-bottom: grey dotted 1px;
+    .canvas {
+        width: 100%;
     }
 
-    #wrapper-bottom {
+    .wrapper-top {
+        background-color: white;
+        border-bottom: black dotted 1px;
+    }
+
+    .wrapper-bottom {
         background-color: black;
-        margin: 0;
-        padding: 0;
-        width: 30px;
-        position: absolute;
-        bottom: 0;
-        border-top: #606060 dotted 1px;
+        border-top: white dotted 1px;
     }
 </style>
