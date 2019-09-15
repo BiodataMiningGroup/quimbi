5bd5dc36c9ac6b37874b74189736aa0aa9bbacc0
Christian <cfortmann@techfak.uni-bielefeld.de>
2019-09-15 02:02:41 +0200

    call me hackerman

--- src/components/ROIs.vue
+++ src/components/ROIs.vue
@@ -45,32 +45,30 @@
 
 <template>
 
-<div id="sidebar">
-    <vs-sidebar :reduce="reduce" :reduce-not-hover-expand="notExpand" parent="body" default-index="1" color="success" class="sidebarx" spacer v-model="active">
-        <div class="header-sidebar" slot="header">
-            <p>Interesting Regions</p>
-        </div>
-        <vs-divider icon-pack="fa" icon="fa fa-draw-polygon" position="center">Map Regions</vs-divider>
+<vs-sidebar :reduce="reduce" :reduce-not-hover-expand="notExpand" parent="body" default-index="1" color="success" class="sidebarx" spacer v-model="active" id="sidebar">
+    <div class="header-sidebar" slot="header">
+        <p>Interesting Regions</p>
+    </div>
+    <vs-divider icon-pack="fa" icon="fa fa-draw-polygon" position="center">Map Regions</vs-divider>
 
-        <vs-sidebar-group open title="Map Regions">
-            <div v-for="(mapROI, index) in mapROIs">
-                <vs-sidebar-group close :title="'Region ' + index">
-                    <vs-button icon-pack="fa" icon="fa-eye" color="danger" type="flat" @click="visibilityArea(mapROI.coords.toString())">show</vs-button>
-                    <vs-button icon-pack="fa" icon="fa-trash" color="danger" type="flat" @click="removeArea(mapROI.coords.toString())">remove</vs-button>
-                </vs-sidebar-group>
-            </div>
-        </vs-sidebar-group>
-        <vs-divider icon-pack="fa" icon="fa fa-chart-area" />
-        <vs-sidebar-group open title="Spectral Regions">
-            <div v-for="spectralROI in spectralROIs">
-                <vs-sidebar-group close :title="spectralROI.id[0] + '-' + spectralROI.id[1]">
-                    <vs-button icon-pack="fa" icon="fa-eye" color="danger" type="flat" @click="visibilitySpectrum(spectralROI.id.toString())">show</vs-button>
-                    <vs-button icon-pack="fa" icon="fa-trash" color="danger" type="flat" @click="removeSpectrum(spectralROI.id.toString())">remove</vs-button>
-                </vs-sidebar-group>
-            </div>
-        </vs-sidebar-group>
-    </vs-sidebar>
-</div>
+    <vs-sidebar-group open title="Map Regions">
+        <div v-for="(mapROI, index) in mapROIs">
+            <vs-sidebar-group close :title="'Region ' + index">
+                <vs-button icon-pack="fa" icon="fa-eye" color="danger" type="flat" @click="visibilityArea(mapROI.coords.toString())">show</vs-button>
+                <vs-button icon-pack="fa" icon="fa-trash" color="danger" type="flat" @click="removeArea(mapROI.coords.toString())">remove</vs-button>
+            </vs-sidebar-group>
+        </div>
+    </vs-sidebar-group>
+    <vs-divider icon-pack="fa" icon="fa fa-chart-area" />
+    <vs-sidebar-group open title="Spectral Regions">
+        <div v-for="spectralROI in spectralROIs">
+            <vs-sidebar-group close :title="spectralROI.id[0] + '-' + spectralROI.id[1]">
+                <vs-button icon-pack="fa" icon="fa-eye" color="danger" type="flat" @click="visibilitySpectrum(spectralROI.id.toString())">show</vs-button>
+                <vs-button icon-pack="fa" icon="fa-trash" color="danger" type="flat" @click="removeSpectrum(spectralROI.id.toString())">remove</vs-button>
+            </vs-sidebar-group>
+        </div>
+    </vs-sidebar-group>
+</vs-sidebar>
 
 </template>
 
@@ -88,6 +86,12 @@ export default {
             reduce: true,
         }
     },
+    mounted(){
+        this.setVSSidebarGroupLogo();
+    },
+    updateds() {
+        this.setVSSidebarGroupLogo();
+    },
     methods: {
         removeSpectrum(id) {
                 this.$emit("removespectrum", id);
@@ -102,8 +106,17 @@ export default {
                 this.$emit("visibilityarea", id);
 
             },
+            setVSSidebarGroupLogo() {
+                for (let x of document.getElementById("sidebar").getElementsByClassName("vs-sidebar-group")) {
+                    let element = x.getElementsByTagName("i")[0];
+                    element.classList.remove('material-icons');
+                    element.classList.add('fa');
+                    element.classList.add('fa-chevron-down');
+                    element.innerHTML = "";
+                }
+            }
     }
 
 }
- 
+
 </script>
