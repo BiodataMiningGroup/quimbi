d23d3b5a3ba6b946e753ae7c84ecaed671c5dfb0
dprib <daniel.prib@uni-bielefeld.de>
2026-06-14 17:01:56 +0200

    Refactor backend

--- backend/main.py
+++ backend/main.py
@@ -1,48 +1,78 @@
 import numpy as np
-from math import floor, ceil
-from contextlib import asynccontextmanager
-from fastapi import FastAPI
-from fastapi.responses import Response
-
-regions = []
-
-def load_regions(npz_file):
-    global regions
-
-    dataset = np.load(npz_file)
-    data = dataset["data"]
-    global_max = data.reshape(-1).max()
-    global_min = data.reshape(-1).min()
-
-    regionHeight = data.shape[0]
-    regionWidth = data.shape[1]
-    regionSizeUpperLimit = 1000
-    while regionWidth * regionHeight > regionSizeUpperLimit:
-        regionWidth = regionWidth / 2
-        regionHeight = regionHeight / 2
-    regionWidth = ceil(regionWidth)
-    regionHeight = ceil(regionHeight)
-
-    for y in range(0, data.shape[0], regionHeight):
-        for x in range(0, data.shape[1], regionWidth):
-            region = data[y:y+regionHeight, x:x+regionWidth, :]
-            region = (region - global_min) / (global_max - global_min)
-            regions.append(region)
+import h5py
+from math import ceil
+from fastapi import FastAPI, Response
 
 
-@asynccontextmanager
-async def lifespan(app: FastAPI):
-    load_regions("../data/20180406_AF_DHB_RP_hum_Skin_PXE_116_3_neu_autopicked.npz")
-    yield
-    app.state.my_service.close()
+class RegionHandler:
+    H5_FILE = "regions.h5"
+    NPZ_FILE = "../data/20180406_AF_DHB_RP_hum_Skin_PXE_116_3_neu_autopicked.npz"
+
+    def __init__(self):
+        self.num_regions = 0
+        self.load_regions()
+
+    def load_regions(self):
+        dataset = np.load(self.NPZ_FILE)
+        data = dataset["data"]
+        global_max = data.max()
+        global_min = data.min()
+
+        regionHeight = data.shape[0]
+        regionWidth = data.shape[1]
+        regionSizeUpperLimit = 1000
+
+        while regionWidth * regionHeight > regionSizeUpperLimit:
+            regionWidth /= 2
+            regionHeight /= 2
+
+        regionWidth = ceil(regionWidth)
+        regionHeight = ceil(regionHeight)
+
+        with h5py.File(self.H5_FILE, "w") as h5f:
+            i = 0
+            for y in range(0, data.shape[0], regionHeight):
+                for x in range(0, data.shape[1], regionWidth):
+                    region = data[y:y + regionHeight, x:x + regionWidth, :]
+                    region = (region - global_min) / (global_max - global_min)
+                    h5f.create_dataset(
+                        f"region_{i}",
+                        data=region,
+                        compression="gzip"
+                    )
+                    i += 1
+            self.num_regions = i
+
+    def get_region(self, index):
+        if index < 0 or index >= self.num_regions:
+            return None
+
+        with h5py.File(self.H5_FILE, "r") as h5f:
+            return h5f[f"region_{index}"][:]
 
 
-app = FastAPI(lifespan=lifespan)
+class ChannelImageHandler:
+    pass
+
+
+regionHandler = RegionHandler()
+channelImageHandler = ChannelImageHandler()
+app = FastAPI()
 
 
 @app.get("/spectrum")
 def get_spectrum(index: int):
+    region = regionHandler.get_region(index)
+    if region is None:
+         return Response(status_code=404)
+
     return Response(
-        content=regions[index].tobytes(),
+        content=region.tobytes(),
         media_type="application/octet-stream",
     )
+
+"""
+@app.get("/channel")
+def get_channel(mz: float):
+    pass
+"""
