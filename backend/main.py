2131a475ddd1a905d34012b959447919794db4be
dprib <daniel.prib@uni-bielefeld.de>
2026-07-02 11:38:49 +0200

    Add endpoint for requesting m/z-images

--- backend/main.py
+++ backend/main.py
@@ -52,7 +52,31 @@ class RegionHandler:
 
 
 class ChannelImageHandler:
-    pass
+    NPZ_FILE = "../data/20180406_AF_DHB_RP_hum_Skin_PXE_116_3_neu_autopicked.npz"
+
+    def __init__(self):
+        with np.load(self.NPZ_FILE) as npz_file:
+            self.data = npz_file['data']
+            self.channels = npz_file['channels'].tolist()
+
+    def get_channel_image(self, index):
+        if index < 0 or index >= self.data.shape[2]:
+            return None
+
+        # Select m/z-channel
+        data = self.data[:, :, index]
+
+        # Normalize intensities to [0, 255]
+        uint8_max = np.iinfo(np.uint8).max
+        max_val = data.reshape(-1).max()
+        min_val = data.reshape(-1).min()
+        data = np.round((data - min_val) / (max_val - min_val) * uint8_max).astype(np.uint8)
+
+        # Fill all color channels (RGBA) with same image
+        data = data.ravel()
+        data = np.repeat(data, 4)
+
+        return data
 
 
 regionHandler = RegionHandler()
@@ -71,8 +95,15 @@ def get_spectrum(index: int):
         media_type="application/octet-stream",
     )
 
-"""
+
 @app.get("/channel")
-def get_channel(mz: float):
-    pass
-"""
+def get_channel(index: float):
+    image = channelImageHandler.get_channel_image(int(index))
+    if image is None:
+        return Response(status_code=404)
+
+    return Response(
+        content=image.tobytes(),
+        media_type="application/octet-stream",
+    )
+
