b4ad4b9ff2faa8c577d86ba4ec21299f5c644aad
dprib <daniel.prib@uni-bielefeld.de>
2026-07-16 23:00:07 +0200

    Add threshold to 2bit-quantization

--- scripts/dataset-zip-creator.py
+++ scripts/dataset-zip-creator.py
@@ -9,8 +9,7 @@ import io
 class ZipCreator(object):
     def __init__(self, file, name='', precision=8, overlay=None, threshold=0):
         self.file = os.path.abspath(file)
-        #self.out_file = '{}.{}.zip'.format(self.file, precision)
-        self.out_file = f"{self.file}.{precision}{'-TH'+str(threshold) if precision == 1 else ''}.zip"
+        self.out_file = f"{self.file}.{precision}{'-TH'+str(threshold) if precision in (1,2) else ''}.zip"
         self.name = os.path.basename(file) if name == '' else name
         self.precision = precision
         self.overlay = overlay
@@ -40,14 +39,9 @@ class ZipCreator(object):
 
     def create2_(self, data, zip_file, metadata):
         uint8_max = np.iinfo(np.uint8).max
-        q0, q1, q2, q3 = np.quantile(data[data > 0], [0, 1/3, 2/3, 1])
-
-        print("x = 0: " , np.sum((data == 0) / data.size * 100), "%")
-        print("0 < x: " , np.sum((data > 0) / data.size * 100), "%")
-        print(q0, q1, q2, q3)
-        print("q0 <= x < q1: ", np.sum((q0 <= data) & (data < q1)) / data.size * 100, "%")
-        print("q1 <= x < q2: ", np.sum((q1 <= data) & (data < q2)) / data.size * 100, "%")
-        print("q2 <= x < q3: ", np.sum((q2 <= data) & (data < q3)) / data.size * 100, "%")
+        global_max = data.max()
+        intensity_threshold = global_max * self.threshold * 0.01
+        q0, q1, q2, q3 = np.quantile(data[data > intensity_threshold], [0, 1/3, 2/3, 1])
 
         # Add missing feature channels to make shape[2] divisible by 16.
         if data.shape[2] % 16 != 0:
@@ -170,7 +164,7 @@ if __name__ == '__main__':
     parser.add_argument("-n", "--name", dest='name', type=str, default='', help="optional dataset name")
     parser.add_argument('-p', '--precision', dest='precision', choices=[1, 2, 8, 16, 32], default=8, type=int, help='bit precision to store the dataset in (default: 8)')
     parser.add_argument('-o', '--overlay', type=str, help='optional path to the overlay image')
-    parser.add_argument('-t', '--threshold', dest='threshold', type=float, default=0, help='% threshold (of max. intensity) for 1-bit quantization')
+    parser.add_argument('-t', '--threshold', dest='threshold', type=float, default=0, help='% threshold (of max. intensity) for 1-bit and 2-bit quantization')
     args = vars(parser.parse_args())
 
     creator = ZipCreator(**args)
